b2f0ce7fc8a8a36a6431e85efc4fb6b50b9a9b69
leo salomao <leosalomao20@gmail.com>
2023-11-28 02:59:32 -0300

    database configuration and first migration

---
+++ erp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=30, null=False, blank=False)
+    sobrenome = models.CharField(max_length=70, null=False, blank=False)
+    cpf = models.CharField(max_length=14, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    remuneracao = models.DecimalField(
+        max_digits=8, decimal_places=2, null=False, blank=False
+    )
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50, null=False, blank=False)
+    descricao = models.CharField(max_length=256, null=False, blank=False)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
+
+
+class Venda(models.Model):
+    funcionario = models.ForeignKey("Funcionario", on_delete=models.CASCADE)
+    produto = models.ForeignKey("Produto", on_delete=models.CASCADE)
+    data_hora = models.DateTimeField(auto_now_add=True)
